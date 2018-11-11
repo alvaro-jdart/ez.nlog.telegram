30300fa31da530820e6a286360df1ca3095b8c73
ez <ez@worldcore.eu>
2018-11-11 17:05:27 +0300

    Added code

---
+++ TelegramClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ez.NLog.Telegram
+{
+    internal class TelegramClient
+    {
+        private const string okTemplate = "\"ok\":true";
+
+        private readonly HttpClient _httpClient = new HttpClient();
+        private readonly string _baseUrl;
+
+        public TelegramClient(string host, string botToken)
+        {
+            _baseUrl = host + botToken;
+        }
+
+        public async Task<(bool isOk, string response)> SendMessage(string chatId, string message)
+        {
+            var url = $"{_baseUrl}/sendMessage?chat_id={chatId}&text={WebUtility.UrlEncode(message)}";
+            var response = await Get(url);
+            return (response.Contains(okTemplate), response);
+        }
+
+        private async Task<string> Get(string url)
+        {
+            using (var response = await _httpClient.GetAsync(url).ConfigureAwait(false))
+            {
+                return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ TelegramTarget.cs
@@ -0,0 +1,44 @@
+﻿using NLog;
+using NLog.Common;
+using NLog.Config;
+using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ez.NLog.Telegram
+{
+    [Target("Telegram")]
+    public class TelegramTarget : AsyncTaskTarget
+    {
+        [RequiredParameter]
+        public string HostUrl { get; set; } = "https://api.telegram.org/bot";
+
+        [RequiredParameter]
+        public string BotToken { get; set; }
+
+        [RequiredParameter]
+        public string ChatId { get; set; }
+
+        private readonly Lazy<TelegramClient> _telegramClient;
+
+        public TelegramTarget()
+        {
+            _telegramClient = new Lazy<TelegramClient>(() => new TelegramClient(HostUrl, BotToken));
+        }
+
+        protected override async Task WriteAsyncTask(LogEventInfo logEvent, CancellationToken cancellationToken)
+        {
+            var message = Layout.Render(logEvent);
+
+            var (isOk, response) = await _telegramClient.Value.SendMessage(ChatId, message);
+
+            if (!isOk)
+            {
+                throw new Exception($"Telegram api error. Response: {response}");
+            }
+        }
+    }
+}
